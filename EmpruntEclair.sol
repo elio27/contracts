ef647d74aabcc9d5b32a02a71da1e65a2f4a7430
elio27 <67876496+elio27@users.noreply.github.com>
2022-04-30 10:20:40 +0200

    Update EmpruntEclair.sol

--- EmpruntEclair.sol
+++ EmpruntEclair.sol
@@ -3,20 +3,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 interface IEmprunteur {
     function doStuff(
+        address _nftContract,
         uint256[] calldata _ids,
         uint256[] calldata _amounts,
-        bytes calldata params
+        bytes calldata _params
         ) external;
 }
 
 contract EmpruntEclair {
 
-    function unsafeIncrement(uint8 i) internal pure returns(uint8) {
+    function unsafeIncrement(uint256 i) internal pure returns(uint256) {
         unchecked {
             return i+1;
         }
@@ -33,14 +33,38 @@ contract EmpruntEclair {
     }
 
     function _flashloan721(address _nftContract, address _executor, uint[] calldata _ids, bytes calldata _params) internal {
-        uint8 len = uint8(_ids.length);
-        for (uint8 i; i<len; i=unsafeIncrement(i)) {
-            IERC721(_nftContract).transferFrom(address(this), _executor, _ids[i]);
+        uint256 len = uint256(_ids.length);
+        IERC721 _contract = IERC721(_nftContract);
+
+        // Envoie les NFTs voulus au contrat executeur
+        for (uint256 i; i<len; i=unsafeIncrement(i)) {
+            _contract.transferFrom(address(this), _executor, _ids[i]);
         }
+
+        // Execute le code du flashloan
+        IEmprunteur(_executor).doStuff(_nftContract, _ids, _ids, _params);
+
+        // Verifie que les NFTs sont bien revenus au contrat et les reprend si ce n'est pas le cas
+        for (uint256 i; i<len; i=unsafeIncrement(i)) {
+            if (_contract.ownerOf(_ids[i]) != address(this)) {
+                _contract.safeTransferFrom(_executor, address(this), _ids[i]);
+            }
+        }
+
     }
 
     function _flashloan1155(address _nftContract, address _executor, uint[] calldata _ids, uint[] calldata _amounts, bytes calldata _params) internal {
-        IERC1155(_nftContract).safeBatchTransferFrom(address(this), _executor, _ids, _amounts,"0x0");
+
+        IERC1155 _contract = IERC1155(_nftContract);
+
+        // Envoie les NFTs voulus au contrat executeur
+        _contract.safeBatchTransferFrom(address(this), _executor, _ids, _amounts,"0x0");
+
+        // Execute le code du flashloan
+        IEmprunteur(_executor).doStuff(_nftContract, _ids, _amounts, _params);
+
+        // Recupere les NFTs empruntés
+        _contract.safeBatchTransferFrom(_executor, address(this), _ids, _amounts, "0x0");
     }
 
 }
